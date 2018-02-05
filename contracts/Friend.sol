ffb19b2f3dd2d46c22ea3b57692f10cf62709304
marcpre <marcus.presich@gmail.com>
2018-02-05 21:16:21 +0100

    fixing gitignore and contract

---
+++ contracts/Friend.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+contract Friend {
+
+    string name;
+    uint age;
+
+    function setFriend(string _name, uint _age) public {
+        name = _name;
+        age = _age;
+    }
+
+    function getFriend() public constant returns (string, uint) {
+        return(name, age);
+    }
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var Friend = artifacts.require("Friend");
+
+module.exports = function(deployer) {
+  deployer.deploy(Friend);
+};
