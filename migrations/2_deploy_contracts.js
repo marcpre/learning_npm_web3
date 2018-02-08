a0af494180f16e30735ddde8a45c7bf0c16c50c7
marcpre <marcus.presich@gmail.com>
2018-02-08 17:17:50 +0100

    adding simple wallet contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-var Friend = artifacts.require("Friend");
-
+var Friend = artifacts.require("./Friend");
+var SimpleWallet = artifacts.require("./SimpleWallet.sol");
 module.exports = function(deployer) {
   deployer.deploy(Friend);
+  deployer.deploy(SimpleWallet);
 };
